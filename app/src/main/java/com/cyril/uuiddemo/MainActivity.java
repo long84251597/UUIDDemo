80eb7039655240fde11d62d7984415f8fb31fcd1
bigsmile <dreamisleader@sina.com>
2019-05-09 16:37:49 +0800

    first commit

---
+++ app/src/main/java/com/cyril/uuiddemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.cyril.uuiddemo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv, tv1;
+    private TelephonyManager manager;
+    private String imei;
+    private DeviceUuidFactory uuidFactory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textview);
+        tv1 = (TextView) findViewById(R.id.textview1);
+        manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        imei = manager.getDeviceId();
+        tv.setText(imei);
+        uuidFactory = new DeviceUuidFactory(this);
+        tv1.setText(uuidFactory.getUuid() + "");
+    }
+}
